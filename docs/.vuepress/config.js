e40313ac3de2bdacf632298997595356a010cac1
leich <453180262@qq.com>
2024-03-13 16:51:44 +0800

    onlyfanfs

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,33 +1,35 @@
 module.exports = {
-  base:"/HHGuide/",
-  lang: 'zh-CN',
-  title: '雷小鸿',
-  description: '鸿鸿coding',
-  themeConfig: {
-    nav: [
-      {
-        text: '首页',
-        link: '/'
-      },
-      {
-        text: 'ChatGPT',
-        link: '/chatgpt/'
-      },
-      { 
-         text: '知乎|掘金', 
-         items: [
-                    { text: '知乎', link: 'https://www.zhihu.com/people/lei-xiao-hong-67' },
-                    { text: '掘金', link: 'https://juejin.cn/user/2981531267379101' }
-                ]       
-      }
-    ],
-    sidebar: {
-      '/chatgpt/': [
-        {
-          title: 'ChatGPT',
-          children: ['how-to-update-gpt4']
-        }
-      ]
-    }
-  }
+	lang: 'zh-CN',
+	title: 'ChatGPT小站',
+	description: 'ChatGPT小站',
+	theme: 'vuepress-theme-vdoing',
+	head: [["script", {},
+	`
+	var _hmt = _hmt || []; (function() {
+		var hm = document.createElement("script");
+		hm.src = "https://hm.baidu.com/hm.js?59033874dd991ce9246775fed052092a";
+		var s = document.getElementsByTagName("script")[0];
+		s.parentNode.insertBefore(hm, s);
+	})();`], ["meta", {
+		name: "keywords",
+		content: "ChatGPT升级,Onlyfans订阅"
+	}]],
+	plugins: [['sitemap', {
+		hostname: "https://chatgpt.diqiulian.net",
+		exclude: ["/404.html"]
+	}]],
+	themeConfig: {
+		sidebar: 'auto',
+		nav: [{
+			text: '首页',
+			link: '/'
+		},
+		{
+			text: 'OnlyFans订阅',
+			link: '/onlyfans/'
+		}],
+		sidebar: {
+			'/onlyfans/': ['/']
+		}
+	}
 }
